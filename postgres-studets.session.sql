8163c265fe91fd5819034b363aeff2edda98ea56
Volya23 <v.o.kryzhanivska@gmail.com>
2023-11-24 16:52:32 +0200

    comments

--- postgres-studets.session.sql
+++ postgres-studets.session.sql
@@ -3,17 +3,17 @@ CREATE TABLE test();
 DROP TABLE users;
 
 CREATE TABLE users(
-    first_name varchar(64),
-    last_name varchar (64),
-    biography text,
+    first_name varchar(64) NOT NULL CHECK(first_name !=''),
+    last_name varchar (64), -- NOT NULL - записи зі значенням NULL не будуть додаваться в таблицю
+    biography text, -- CHECK(first_name !='') - не пропускає строки де є умови котрі вказані в дужках
     gender varchar (30),
     is_subscibe boolean,
-    birthday date,
+    birthday date UNIQUE, -- UNIQUE - унікальне значення, наприклад, щоб не було однакових імейлов для різних користувачів
     foot_size smallint,
     height numeric(5, 2)
 );
 
-INSERT INTO users VALUES (
+INSERT INTO users VALUES ( --можна записувати в стовпчик, а також в строчку
     'Donald',
     'Tramp',
     '',
@@ -24,13 +24,4 @@ INSERT INTO users VALUES (
     1.97
 ),
 (
-    'John',
-    'Doe',
-    NULL,
-    'male',
-    true,
-    '1976-01-25',
-    26,
-    2.17
-)
-;
+    'John', 'Doe', NULL, 'male', true, '1976-01-25', 26, 2.17);
