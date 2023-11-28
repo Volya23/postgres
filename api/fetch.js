b6f6f2071c839a1b592f785065d288630a298099
Volya23 <v.o.kryzhanivska@gmail.com>
2023-11-28 22:23:49 +0200

    axios,fetch contine

---
+++ api/fetch.js
@@ -0,0 +1,5 @@
+module.exports.getUsers = async() => {
+    const response = await fetch ('https://randomuser.me/api/?results=1000');
+    const data = await response.json();
+    return data.results;
+}
