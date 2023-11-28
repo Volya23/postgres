409d2d0b4cf361c3c4ea79a4008e7c7a895a7510
Volya23 <v.o.kryzhanivska@gmail.com>
2023-11-28 22:38:52 +0200

    array users

--- index.js
+++ index.js
@@ -1,21 +1,58 @@
 const { Client } = require ('pg');
+const { mapUsers } = require ('./utils')
 const { configs } = require('./configs/index');
 //const { loadUsers } = require('./api');
 const { getUsers } = require('./api/fetch');
 
 const client = new Client(configs);
-
+const usersArray = [
+    {
+        firstName: 'Test1',
+        lastName: 'Grishina',
+        email: 'test1@gmail.com',
+        isSubscribe: false,
+        gender: 'female'
+    },
+    {
+        firstName: 'Test2',
+        lastName: 'Grishina',
+        email: 'test2@gmail.com',
+        isSubscribe: false,
+        gender: 'female'
+    },
+    {
+        firstName: 'Test3',
+        lastName: 'Grishina',
+        email: 'test3@gmail.com',
+        isSubscribe: false,
+        gender: 'female'
+    },
+    {
+        firstName: 'Test4',
+        lastName: 'Grishina',
+        email: 'test4@gmail.com',
+        isSubscribe: false,
+        gender: 'female'
+    },
+    {
+        firstName: 'Test5',
+        lastName: 'Grishina',
+        email: 'test5@gmail.com',
+        isSubscribe: false,
+        gender: 'female'
+    }
+]
 async function start() {
     await client.connect();
 
-    const responce = await client.query(`
+    const response = await client.query(`
         INSERT INTO users(first_name, last_name, email, gender, is_subscibe) VALUES
-        ('Petr', 'Ivanov', 'petr.ivanov@gmail.com', 'male', true);
+        ${mapUsers(usersArray)}
     `)
 
     await client.end();
 
-    console.log(responce);
+    console.log(response);
 }
 
 start();
